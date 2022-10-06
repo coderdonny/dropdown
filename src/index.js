513cc14d5e99a03aaddbda710cfb89175ad664ca
Daniel Karpierz <dankarp1999@gmail.com>
2022-10-06 12:21:47 -0400

    Create javascript toggle dropdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles/style.css';
+
+const jsDropDownBtn = document.querySelector('.dropdown-button-js');
+const jsDropDownMenu = document.querySelector('.dropdown-menu-js');
+
+jsDropDownBtn.addEventListener('click', toggleDropdown);
+
+function toggleDropdown() {
+	if (jsDropDownMenu.classList.contains('show')) {
+		jsDropDownMenu.classList.remove('show');
+	} else {
+		jsDropDownMenu.classList.add('show');
+	}
+}
